395b01df5493e2aa3d52302daaa21af626f7d034
Dark-Light-20 <sebastian.diaz@udenar.edu.co>
2019-11-12 14:32:33 -0500

    Fixed logical operators

--- lexic.py
+++ lexic.py
@@ -1,7 +1,8 @@
 def lexicAnalysis(sequence):
     operators=['+','-','*','/','^','(',')','&','|','<','>','!','=']
     lexicSequence=[]
-    available=False     # available -> to push an "I"
+    available=False     # available -> to push an 'I'
+    available2=False    # validate: (a '=' already analyzed after other operator) or (double logic operator)
     
     for pos, char in enumerate(sequence):   # create a tuple (pos, char) of sequence
         if char.isnumeric():
@@ -10,31 +11,53 @@ def lexicAnalysis(sequence):
             if available:
                 lexicSequence.append("I")
                 available=False
+            if available2:
+                available2=False
+                continue
             if char == '<':
                 if pos < len(sequence)-1 and sequence[pos+1] == '=':
                     lexicSequence.append("<=")
+                    available2=True
                     continue
             elif char == '>':
                 if pos < len(sequence)-1 and sequence[pos+1] == '=':
                     lexicSequence.append(">=")
+                    available2=True
                     continue
             elif char == '!':
                 if pos < len(sequence)-1 and sequence[pos+1] == '=':
                     lexicSequence.append("!=")
+                    available2=True
                     continue
                 else:
                     return error(pos, char)
             elif char == '=':
                 if pos < len(sequence)-1 and sequence[pos+1] == '=':
                     lexicSequence.append("==")
+                    available2=True
                     continue
                 else:
-                    continue        
+                    return error(pos, char)        
+            elif char == '|':
+                if pos < len(sequence)-1 and sequence[pos+1] == '|':
+                    lexicSequence.append("||")
+                    available2=True
+                    continue
+                else:
+                    return error(pos, char)     
+            elif char == '&':
+                if pos < len(sequence)-1 and sequence[pos+1] == '&':
+                    lexicSequence.append("&&")
+                    available2=True
+                    continue
+                else:
+                    return error(pos, char)        
+            
             lexicSequence.append(char)
         else:
             return error(pos, char)
 
-    if available:   # validation if last element in sequence is "I"
+    if available:   # validation if last element in sequence is 'I'
         lexicSequence.append("I")
     
     lexicSequence.append("$")   # end of sequence char
